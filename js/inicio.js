f3c5a3480ee856faf67098f07739f3522c5b138c
Roberto Isla <robertoisla92@gmail.com>
2021-03-30 20:17:56 -0300

    Esqueleto de HTML, css, ts y js, por ultimo, definicion de clase Persona en ts y js

---
+++ js/inicio.js
@@ -0,0 +1,11 @@
+"use strict";
+var Persona = /** @class */ (function () {
+    function Persona(RUT, name, lastname, email, phoneNumber) {
+        this.RUT = RUT;
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+    return Persona;
+}());
